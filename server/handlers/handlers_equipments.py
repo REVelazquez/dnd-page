8c9af05e0193aa67469769611706e195b3c592a5
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2023-12-15 21:05:38 -0300

    equipment details start

--- server/handlers/handlers_equipments.py
+++ server/handlers/handlers_equipments.py
@@ -5,17 +5,33 @@ import os
 
 load_dotenv()
 url_general= os.getenv('GENERAL_URL')
-def equipment_categories_api():
+def equipment_api():
     url = url_general + 'equipment'
     response = requests.get(url)
     try:
-            equipment_categories=[]
+            equipments=[]
             if response.status_code == 200:
                 datos = response.json()
                 if 'results' in datos:
                     resultados = datos ['results'] 
                     for item in resultados:
-                        equipment_categories.append({'index': item['index'], 'name': item['name']})
-            return equipment_categories
+                        equipments.append({'index': item['index'], 'name': item['name']})
+            return equipments
     except Exception as e:
          print('Ocurrio un error', e)
+
+def equipment_details ():
+    equipments=equipment_api()
+    equipments   
+    for equipment in equipments:
+        url=url_general+'equipment/'+equipment['index']
+        response = requests.get(url)
+        try:
+            if response.status_code== 200:
+                temporal_detail = {}
+            
+        except Exception as error:
+             print('An error happened:', error)
+if __name__ == '__main__':
+    equipment_api()
+    
